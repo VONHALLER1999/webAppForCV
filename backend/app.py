f8071492338a479def9a5349d090d4121d1a67a1
VONHALLER1999 <62394387+VONHALLER1999@users.noreply.github.com>
2025-03-25 16:17:21 +0100

    supervisor and port fix

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from calc import simulate_hedging_strategy
 import numpy as np
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS to allow requests from your React app
@@ -28,5 +29,6 @@ def simulate_hedges():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
